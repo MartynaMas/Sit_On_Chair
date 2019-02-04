a5225dcd502b3fdb9affc8ac0340bdce87bd7821
Martyna Masłowska <maslowskamartyna@wp.pl>
2019-02-04 15:36:25 +0100

    const/let

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@
 /* slider */
 /* -------------------------------------------- */
 
-var prev = document.querySelector('.banner-prev');
-var next = document.querySelector('.banner-next');
-var list = document.querySelectorAll('.banner-slide');
-var index = 0;
+const prev = document.querySelector('.banner-prev');
+const next = document.querySelector('.banner-next');
+const list = document.querySelectorAll('.banner-slide');
+let index = 0;
 
 list[index].classList.add('banner-slide-show');
 
 prev.addEventListener('click', function (event) {
     event.preventDefault();
-
     list[index].classList.remove('banner-slide-show');
 
     if (index === 0 ) {
